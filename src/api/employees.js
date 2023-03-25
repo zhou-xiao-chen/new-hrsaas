2e3468b2bb63752131e70cdc0166437b3ff8ec1d
zhouxiaochen <1611454405@qq.com>
2023-03-25 19:39:43 +0800

    员工详情布局完成 更新完成

--- src/api/employees.js
+++ src/api/employees.js
@@ -37,3 +37,41 @@ export function delEmployees (id) {
     method: 'DELETE'
   })
 }
+
+export function saveStaffInfo (data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'PUT',
+    data
+  })
+}
+
+export function getPersonalDetail (id) {
+  return request({
+    url: `/employees/${id}/personalInfo`,
+    method: 'GET'
+  })
+}
+
+export function updatePersonalDetail (data) {
+  return request({
+    url: `/employees/${data.userId}/personalInfo`,
+    method: 'PUT',
+    data
+  })
+}
+
+export function getJobDetail (id) {
+  return request({
+    url: `/employees/${id}/jobs`,
+    method: 'GET'
+  })
+}
+
+export function updateJob (data) {
+  return request({
+    url: `/employees/${data.userId}/jobs`,
+    method: 'PUT',
+    data
+  })
+}
--- src/router/modules/employees.js
+++ src/router/modules/employees.js
@@ -4,11 +4,17 @@ export default {
   path: '/employees',
   name: 'employees',
   component: Layout,
-  children: [{
-    path: '',
-    component: () => import('@/views/employees'),
-    meta: {
-      title: '员工', icon: 'people'
-    }
-  }]
+  children: [
+    { path: '',
+      component: () => import('@/views/employees'),
+      meta: {
+        title: '员工', icon: 'people'
+      }},
+    { path: 'detail/:id',
+      component: () => import('@/views/employees/detail'),
+      hidden: true,
+      meta: {
+        title: '员工详情'
+      }}
+  ]
 }
